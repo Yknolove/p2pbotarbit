89d31dc7ed4a0d17fa935565facbcac85ed94ce2
Yknolove <giffka9@gmail.com>
2025-07-26 16:28:00 +0300

    Update default.py

--- handlers/default.py
+++ handlers/default.py
@@ -1,1 +1,9 @@
+from aiogram import Router, F
+from aiogram.types import Message
+from aiogram.filters import CommandStart
 
+router = Router()
+
+@router.message(CommandStart())
+async def cmd_start(message: Message) -> None:
+    await message.answer("Привет! Используй /filter, чтобы задать пороги.")
